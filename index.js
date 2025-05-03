f0fabd096d96e91e969cd3594cc5c1bbf70a9cde
Midaco-YT <tristancassin20@gmail.com>
2025-05-03 18:02:12 +0200

    Revert "Merge branch 'master' of https://github.com/MidacoYT/ValeriumLauncher"
    This reverts commit 186929e1f181ea2de45ef1a21fd297cf0171baa1, reversing
    changes made to e9c083fd952a888b557e61f76166f81f0fde1c96.

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ ipcMain.on(MSFT_OPCODE.OPEN_LOGIN, (ipcEvent, ...arguments_) => {
         width: 520,
         height: 600,
         frame: true,
-        icon: getPlatformIcon('SealCircle')
+        icon: getPlatformIcon('icon')
     })
 
     msftAuthWindow.on('closed', () => {
@@ -183,7 +183,7 @@ ipcMain.on(MSFT_OPCODE.OPEN_LOGOUT, (ipcEvent, uuid, isLastAccount) => {
         width: 520,
         height: 600,
         frame: true,
-        icon: getPlatformIcon('SealCircle')
+        icon: getPlatformIcon('icon')
     })
 
     msftLogoutWindow.on('closed', () => {
@@ -229,11 +229,12 @@ function createWindow() {
     win = new BrowserWindow({
         width: 1280,
         height: 720,
-        icon: getPlatformIcon('SealCircle'),
-        center: true,
+        icon: getPlatformIcon('icon'),
         frame: false,
         resizable: false,
-        thickFrame: false,
+        fullscreen: false,
+        center: true,
+        maximizable: false,
         webPreferences: {
             preload: path.join(__dirname, 'app', 'assets', 'js', 'preloader.js'),
             nodeIntegration: true,
