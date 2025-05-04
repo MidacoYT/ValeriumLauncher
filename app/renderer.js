8b82dda2d3445be5a12683d7e22301710caef7ab
Midaco-YT <tristancassin20@gmail.com>
2025-05-04 16:04:52 +0200

    updated

--- app/renderer.js
+++ app/renderer.js
@@ -9,7 +9,6 @@ window.onload = () => {
     ipcRenderer.send('autoUpdateAction', 'checkForUpdate'); // Demander au processus principal de vérifier les mises à jour
 }
 
-// Écoute des événements envoyés par le processus principal
 ipcRenderer.on('autoUpdateNotification', (event, arg, info) => {
     switch (arg) {
         case 'checking-for-update':
@@ -23,11 +22,55 @@ ipcRenderer.on('autoUpdateNotification', (event, arg, info) => {
         case 'update-downloaded':
             setUpdateStatus(`Mise à jour téléchargée (Version: ${info.version}). Installation en cours...`);
             // Une fois la mise à jour téléchargée, l'installer
-            ipcRenderer.send('autoUpdateAction', 'installUpdateNow');
+            setTimeout(() => {
+                ipcRenderer.send('autoUpdateAction', 'installUpdateNow');
+            }, 3000);  // Délai de 3 secondes avant d'installer la mise à jour
             break;
 
         case 'update-not-available':
             setUpdateStatus("Aucune mise à jour disponible.");
+
+            setTimeout(() => {
+                const checkVersionReady1 = document.getElementById('checkVersion');
+                const mainPage = document.getElementById('main');
+
+                if (checkVersionReady1 && mainPage) {
+                    checkVersionReady1.style.display = 'none';  // Cacher l'écran de vérification des mises à jour
+                    mainPage.style.display = 'block';  // Afficher la page principale
+
+                    // Ajouter le conteneur de chargement avant d'afficher la page principale
+                    const loadingContainer = document.getElementById('loadingContainer');
+                    if (loadingContainer) {
+                        loadingContainer.style.display = 'flex';  // Afficher le conteneur de chargement
+                    }
+
+                    // Masquer le conteneur de chargement après 3 secondes
+                    setTimeout(() => {
+                        if (loadingContainer) {
+                            loadingContainer.style.display = 'none';  // Masquer le conteneur de chargement après 3 secondes
+                        }
+                    }, 5000);
+                }
+            }, 5000);  // Délai de 3 secondes avant de changer l'affichage
+
+            break;
+
+        case 'ready':
+            loggerAutoUpdater.info('Ready for update check.')
+
+            // Afficher le statut dans la section checkVersion
+            const checkVersionReady = document.getElementById('checkVersion');
+            const checkVersionTextReady = document.getElementById('checkVersionText');
+            if (checkVersionReady && checkVersionTextReady) {
+                checkVersionTextReady.textContent = 'Prêt à vérifier les mises à jour...';
+                checkVersionReady.style.display = 'flex';
+            }
+
+            // Demande de vérification de la mise à jour toutes les 30 minutes (ou dès le démarrage)
+            updateCheckListener = setInterval(() => {
+                ipcRenderer.send('autoUpdateAction', 'checkForUpdate');
+            }, 1800000); // 30 minutes
+            ipcRenderer.send('autoUpdateAction', 'checkForUpdate'); // Demander immédiatement la vérification de la mise à jour
             break;
 
         case 'realerror':
